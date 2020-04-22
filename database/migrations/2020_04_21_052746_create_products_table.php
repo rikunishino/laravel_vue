2ac3e16c4b24f9f6b89d1d5768714bd5e6c7f929
r.nishino <r.nishino@stonesystem.co.jp>
2020-04-22 09:11:08 +0900

    [add] 商品テーブルのマイグレーション

---
+++ database/migrations/2020_04_21_052746_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('subject_id')->unsigned()->comment('科目ID');
+            $table->string('product_name')->comment('商品名');
+            $table->integer('price')->comment('金額');
+            $table->integer('sort_no')->comment('ソートNo');
+            $table->timestamp('create_ts')->useCurrent();
+            $table->timestamp('update_ts')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->boolean('is_deleted')->default(false)->comment('削除フラグ');
+            $table->foreign('subject_id')->references('id')->on('subjects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
